ebc4cd9894b1c1a34a00afbc01a93667fcd81423
rodney.beede@gmail.com <rodney.beede@gmail.com@5f64c388-b88e-07c2-f792-380a126569ba>
2011-03-28 18:24:49 +0000

    git-svn-id: http://pzapr.googlecode.com/svn@30 5f64c388-b88e-07c2-f792-380a126569ba

--- trunk/src/alphanumeric.cpp
+++ trunk/src/alphanumeric.cpp
@@ -3,11 +3,38 @@
 
 std::string currPassword_ALPHANUMERIC;
 
+std::string lastPassword_ALPHANUMERIC;
+
 
 void initializePasswordGenerator_brute(const int rank, const int numProcesses) {
 	
 }
 
+
 std::string getNextPassword_brute() {
-	return std::string("");
+	const string currValue = currPassword_ALPHANUMERIC;
+	
+	// Increment for next time around
+	incrementPassword();
+	
+	return currValue;
+}
+
+
+void incrementPassword() {
+	if(currPassword_ALPHANUMERIC == lastPassword_ALPHANUMERIC) {
+		// Reached end of range so flag it so
+		currPassword_ALPHANUMERIC = string("");
+	}
+	
+	if(string("") == currPassword_ALPHANUMERIC) {
+		return string("");
+	}
+	
+	
+}
+
+
+void incrementCharacter(const int position) {
+	
 }
--- trunk/src/alphanumeric.h
+++ trunk/src/alphanumeric.h
@@ -8,6 +8,12 @@
 
 #include <string>
 
+
+const int MAX_PASSWORD_LENGTH = 7;
+
+
+void incrementCharacter(const int position);
+void incrementPassword();
 void initializePasswordGenerator_brute(const int rank, const int numProcesses);
 std::string getNextPassword_brute();
 
