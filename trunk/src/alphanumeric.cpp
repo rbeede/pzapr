a734246c469a8c30120920b98c230ec77650e4c8
rodney.beede@gmail.com <rodney.beede@gmail.com@5f64c388-b88e-07c2-f792-380a126569ba>
2011-04-01 17:00:43 +0000

    Added project docs
    git-svn-id: http://pzapr.googlecode.com/svn@41 5f64c388-b88e-07c2-f792-380a126569ba

--- trunk/src/alphanumeric.cpp
+++ trunk/src/alphanumeric.cpp
@@ -9,8 +9,8 @@ std::string lastPassword_ALPHANUMERIC;
 
 
 void initializePasswordGenerator_brute(const int rank, const int numProcesses) {
-	const long numPossiblePasswords = calculateNumberPossible(POSSIBLE_PASSWORD_CHARS, MAX_PASSWORD_LENGTH);
-	logger->log("Number of possible passwords when there are " + to_string(POSSIBLE_PASSWORD_CHARS) + 
+	const long numPossiblePasswords = calculateNumberPossible(NUM_POSS_PW_ALPHABET, MAX_PASSWORD_LENGTH);
+	logger->log("Number of possible passwords when there are " + to_string(NUM_POSS_PW_ALPHABET) + 
 				" possible character values and passwords of up to " + to_string(MAX_PASSWORD_LENGTH) +
 				" length is " + to_string(numPossiblePasswords));
 	
@@ -83,11 +83,24 @@ long calculateNumberPossible(const int possibleCombinations, const int character
 }
 
 
-
+/**
+ *	This algorithm allows us to determine a password for an arbitrary position in a range of passwords.
+ *
+ *	Let position represent the placement of a password in a range of 1 to "all possible passwords of length 1 to MAX_PASSWORD_LENGTH"
+ *
+ *	Let PASSWORD_ALPHABET represent all the possible characters that can be used in a password with the special requirement
+ *		that PASSWORD_ALPHABET[0] = '\0'.
+ *	The array indexes represent possible numerical factors that will be used in factoring position later.  The 0 factor
+ *		is the '\0' character which is required since we can't use factors of 0 later.
+ *
+ *	
+ *
+ *
+*/
 std::string passwordFromRangePosition(const long position) {
 	// Figure out how long the password must be at the given position
 	int passwordLength = MAX_PASSWORD_LENGTH;  // assume the worst
-	while(calculateNumberPossible(POSSIBLE_PASSWORD_CHARS, passwordLength) >= position) {
+	while(calculateNumberPossible(NUM_POSS_PW_ALPHABET, passwordLength) >= position) {
 		passwordLength--;
 	}
 	passwordLength++;
@@ -95,9 +108,9 @@ std::string passwordFromRangePosition(const long position) {
 	string password("");
 	
 	
-	// Holds character index position in PASSWORD_CHARS for factors c1, c2, c3, etc...
+	// Holds character index position in PASSWORD_ALPHABET for factors c1, c2, c3, etc...
 	//	Last element holds first character index (aka factor), next last is second, etc until first element which is
-	//	last character index in PASSWORD_CHARS
+	//	last character index in PASSWORD_ALPHABET
 	int factors[passwordLength];
 	for(int i = 0; i < passwordLength; i++) {
 		factors[i] = 0;
@@ -106,11 +119,11 @@ std::string passwordFromRangePosition(const long position) {
 	for(int j = passwordLength - 1; j >= 0; j--) {
 		long runningTotal = 0;
 		for(int i = passwordLength - 1; i > j; i--) {
-			runningTotal += factors[i] * pow(POSSIBLE_PASSWORD_CHARS, i);
+			runningTotal += factors[i] * pow(NUM_POSS_PW_ALPHABET, i);
 		}
 		long remainder = position - runningTotal;
 	
-		int factor = remainder / pow(POSSIBLE_PASSWORD_CHARS, j);
+		int factor = remainder / pow(NUM_POSS_PW_ALPHABET, j);
 
 		// Did we hit an edge where we may be rolling over?
 		if(0 == factor) {
@@ -122,7 +135,7 @@ std::string passwordFromRangePosition(const long position) {
 				factors[prevFacIdx]--;
 				if(factors[prevFacIdx] == 0) {
 					// Borrow one from next factor
-					factors[prevFacIdx] = POSSIBLE_PASSWORD_CHARS;  // set borrowed amount
+					factors[prevFacIdx] = NUM_POSS_PW_ALPHABET;  // set borrowed amount
 					continue;  // next loop around will adjust next factor we borrowed from
 				} else {
 					// Done borrowing
@@ -141,7 +154,7 @@ std::string passwordFromRangePosition(const long position) {
 	}
 	
 	for(int i = passwordLength - 1; i >= 0; i--) {
-		password.append(1, PASSWORD_CHARS[factors[i]]);
+		password.append(1, PASSWORD_ALPHABET[factors[i]]);
 	}
 	
 
--- trunk/src/alphanumeric.h
+++ trunk/src/alphanumeric.h
@@ -18,15 +18,15 @@ const int MAX_PASSWORD_LENGTH = 7;
 
 // All the possible password characters we try for brute force, this mapping makes it easier to do ranges
 // Note that null character isn't really used as a possible valid character in the password, just the string terminator
-const char PASSWORD_CHARS[] = {'\0',  // null character must be at index 0
+const char PASSWORD_ALPHABET[] = {'\0',  // null character must be at index 0
 	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
 	'X', 'Y', 'Z',
 	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
 	'x', 'y', 'z'
 	};
-const int PASSWORD_CHARS_length = 63;  // get this right
-const int POSSIBLE_PASSWORD_CHARS = PASSWORD_CHARS_length - 1;  // Exclude null character
+const int PASSWORD_ALPHABET_length = 63;  // get this right
+const int NUM_POSS_PW_ALPHABET = PASSWORD_ALPHABET_length - 1;  // Exclude null character
 
 
 void incrementCharacter(const int position);
