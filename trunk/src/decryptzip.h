470090a7220135eb42daa5cf22fa7c77f5cef320
agrawal.neelam268@gmail.com <agrawal.neelam268@gmail.com@5f64c388-b88e-07c2-f792-380a126569ba>
2011-04-09 07:35:31 +0000

    Header files added
    git-svn-id: http://pzapr.googlecode.com/svn@80 5f64c388-b88e-07c2-f792-380a126569ba

--- trunk/src/decryptzip.h
+++ trunk/src/decryptzip.h
@@ -1,21 +1,51 @@
 /*
+
  * Neelam
+
  *
+
 */
 
+
+
 #ifndef _DECRYPTZIP_H
+
 #define _DECRYPTZIP_H
 
+
+
 #include <string>
+
 #include <fstream>
+
 #include <iostream>
 
+#include <malloc.h>
+
+#include <stdlib.h>
+
+#include <memory.h>
+
+
+
 #include "stringutils.h"
+
 #include "logger.h"
+
+#include "rijndael.h"
+
+#include "fileenc.h"
+
+
 
 void initDecryptEngine(const char * const zipFilePathname);
 
-bool attemptPassword(const std::string password);
+
+
+bool attemptPassword(const char * const zipFilePathname,const std::string password);
+
+
+
 
 
 #endif /* _DECRYPTZIP_H */
