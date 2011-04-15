3207935526f271e5decc901b816b2e4dc5bc46cd
ysviru <ysviru@5f64c388-b88e-07c2-f792-380a126569ba>
2011-04-15 17:37:33 +0000

    Code cleaned!
    git-svn-id: http://pzapr.googlecode.com/svn@107 5f64c388-b88e-07c2-f792-380a126569ba

--- trunk/src/dictionary.cpp
+++ trunk/src/dictionary.cpp
@@ -52,63 +52,74 @@ void get_Displacement_of_Each_Word(std::ifstream &myfile, int * dispWords)
 }
 
 
-void initializePasswordGenerator_dictionary(const int rank, const int numProcesses, const char * const dictionaryFilePathname) {
+void initializePasswordGenerator_dictionary(const int rank, const int numProcesses, const char * const dictionaryFilePathname) 
+{
 	int tempval;
-	ifstream fileHandle1, fileHandle2;
 
 	dictionaryFileHandle.open(dictionaryFilePathname, ios::in);
-	fileHandle1.open(dictionaryFilePathname, ios::in);
-	fileHandle2.open(dictionaryFilePathname, ios::in);
 	
 	logger->log(to_string(rank) + ": Reading dictionary file at " + to_string(dictionaryFilePathname));
 
-	// Calculate total words in the dictionary file
-	if(!dictionaryFileHandle.is_open()) {
+	if(!dictionaryFileHandle.is_open()) 
+	{
 		logger->log("ERROR OPENING DICTIONARY FILE");
 		MPI_Abort( MPI_COMM_WORLD, 255 );
 		return;
 	}
 	
-	const int totalWords = count_Number_Of_Words(fileHandle1);
-	
+
+	/*------Counts the total number of words in the dictionary file------*/
+	const int totalWords = count_Number_Of_Words(dictionaryFileHandle);
+	dictionaryFileHandle.seekg(0, ios::bed);
+	dictionaryFileHandle.clear();
+	/*-------------------------------------------------------------------*/
+
+
 	logger->log("There were " + to_string(totalWords) + " total dictionary words");
 	
-	fileHandle1.close();
-
 
 	perProcess_WordCount = totalWords / numProcesses;
 	tempval = totalWords % numProcesses;
 
-	
 	int* dispWords = new int[totalWords];
 	
-	/*Find the displacement of each word from the start of the file*/
-	//dictionaryFileHandle.open(dictionaryFilePathname, ios::in);
+	/*----Finds the displacement of each word from the start of the file----*/
 	get_Displacement_of_Each_Word(fileHandle2, dispWords);
-	fileHandle2.close();
+	dictionaryFileHandle.seekg(0, ios::bed);
+	dictionaryFileHandle.clear();
+	/*----------------------------------------------------------------------*/
 
-	/*If the number of passwords not divisible by the number of processes, 
-	then evenly distribute the remaining ones*/
+
+	/* If the number of passwords not divisible by the number of processes, 
+	 * then evenly distribute the remaining ones.
+	 * Important to balance load evenly amongst all the processes*/
 	if(rank<tempval)
 		perProcess_WordCount++;
+	/*-------------------------------------------------------------------*/
 
 
+	/*------------------------------Set the index according to the rank of the processor--------------------------------------------*/
+	/*-------------------This index decides the position in the file from where this processor has to start reading-----------------*/
 	int index = (rank<tempval)?(rank*perProcess_WordCount):((tempval)*(perProcess_WordCount+1) + (rank-tempval)*perProcess_WordCount);
+	/*----------------------------------------------------------------------------------------------------------------------------*/
 
-	/*The starting position for each process is set here*/
+
+
+	/*-----The starting position for each processor is set here-----*/
 	offset = (streampos) dispWords[index];
-	//dictionaryFileHandle.open(dictionaryFilePathname, ios::in);
 	dictionaryFileHandle.seekg(offset);
-	
-	string nextPassword;
+	/*------------------------------------------------------------*/
+
 
 	logger->log("Process " + to_string(rank) + ":  " + "total_words= " + to_string(totalWords) + "No. of Words I will crack: " + to_string(perProcess_WordCount));
 }
 
-std::string getNextPassword_dictionary() {
+std::string getNextPassword_dictionary() 
+{
 	string line("");
 	
-	if(perProcess_WordCount > 0) {
+	if(perProcess_WordCount > 0) 
+	{
 		getline(dictionaryFileHandle, line);
 		perProcess_WordCount--;
 	}
