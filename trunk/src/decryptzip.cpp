af30dd614485ab7e61f60ff1f119c0af345473bf
agrawal.neelam268@gmail.com <agrawal.neelam268@gmail.com@5f64c388-b88e-07c2-f792-380a126569ba>
2011-04-19 04:08:02 +0000

    Added MAC checking code
    git-svn-id: http://pzapr.googlecode.com/svn@136 5f64c388-b88e-07c2-f792-380a126569ba
    
    Former-commit-id: b786d559d28aff85e59313bd805d9aa7d622cf95
    Former-commit-id: 67defd022af5787907f5678a62de4a20fc7ed93e

--- trunk/src/decryptzip.cpp
+++ trunk/src/decryptzip.cpp
@@ -142,12 +142,15 @@ void initDecryptEngine(const char * const zipFilePathname) {
 		// since passwordVerification is an array no need to adjust for little/big endian
 		
 		// next is encrypted data which has length of header.compressedSize
-		// we don't bother with this
+      //comprssed size is total of saltLength, password verifier, encrypted data and MAC
+      verifier_data_object.fileDataSize = (int)header.compressedSize - (saltLengthInBytes + 2 + MAC_LENGTH(mode));
+      verifier_data_object.fileData = (byte *) malloc (sizeof(verifier_data_object.fileDataSize));
+      zipfileStream.read((char *)(&(verifier_data_object.fileData)), verifier_data_object.fileDataSize);
 		
 		// next is authentication code which is 10 bytes
-		// we don't bother with this
+		zipfileStream.read((char *)(&(verifier_data_object.authenticationcode)), MAC_LENGTH(mode));
 		
-		// There is a 1 in 65,536 chance that an incorrect password will yield a matching verification value
+		
 
 	} else {
 		logger->log("ERROR:  ZIP FILE IS NOT AES ENCRYPTED!");
@@ -168,6 +171,24 @@ bool attemptPassword(const std::string password) {
 	if(memcmp(verifier_data_object.passwordVerification,tmp_buf,2)) {
 		return false;	//Password did not match
 	} else {
-		return true; //Password matched
+      byte tmp_buf2[10];
+      int datalength = verifier_data_object.fileDataSize;
+      byte buffer[1024];
+      int position = 0;
+      int len;
+      while(true){
+        len = datalength < 1024 ? datalength : 1024;
+        memcpy(buffer,&verifier_data_object.fileData[position],len);
+        datalength -= len;
+        position += len;
+        fcrypt_decrypt(buffer,len,zcx);
+        if(datalength <= 0)
+          break;
+      }
+      fcrypt_end(tmp_buf2,zcx);
+      if(memcmp(verifier_data_object.authenticationcode,tmp_buf2,MAC_LENGTH(verifier_data_object.mode))){
+        return false;   //Password matched but not MAC
+      }
+		return true; //Password matched as well as MAC
 	}
 }
